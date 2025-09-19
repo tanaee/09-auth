8e053c14edb69f2c1a66f4591c112227be9735eb
tanaee <tanaaeeee@gmail.com>
2025-09-19 16:34:45 +0300

    fixing register route

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -35,12 +35,12 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   } catch (error) {
     if (isAxiosError(error)) {
-      logErrorResponse(error.response?.data);
-      return NextResponse.json(
-        { error: error.message, response: error.response?.data },
-        { status: error.status },
-      );
-    }
+  logErrorResponse(error.response?.data);
+  return NextResponse.json(
+    { error: error.response?.data?.message || error.message },
+    { status: error.response?.status || 500 },
+  );
+}
     logErrorResponse({ message: (error as Error).message });
     return NextResponse.json(
       { error: "Internal Server Error" },
